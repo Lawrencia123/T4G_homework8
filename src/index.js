16db60cb3a8cfc944f78418b150e4ff1f14523b1
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-25 18:33:01 +0000

    made changes

--- src/index.js
+++ src/index.js
@@ -10,18 +10,27 @@
 // }
 // checkGender();
 
-
+// function checkGender() {
+//     let gender = prompt("Please enter your gender (Male or Female):");
+//     // gender will store the value the user enter (Male/Female)
+//    if gender = (gender.toLowerCase() === "female") {
+// alert("You are a girl!")
+//     } 
+//     if (gender == "female");
+//     alert(`You are a ${gender}`);
+// }else if (gender == male) {
+//     alert(`You are a ${gender}`);  
+//     // or alert(`You are a male`)
+// }else{
+//     alert("invalid input. Please enter 'male' or 'female'");
+// }
 function checkGender() {
-    let gender = prompt("Please enter your gender (Male or Female):");
-    // gender will store the value the user enter (Male/Female)
-   if gender = (gender.toLowerCase() === "female") {
-alert("You are a girl!")
-    } 
-    if (gender == "female");
-    alert(`You are a ${gender}`);
-}else if (gender == male) {
-    alert(`You are a ${gender}`);  
-    // or alert(`You are a male`)
-}else{
-    alert("invalid input. Please enter 'male' or 'female'");
+    var gender = prompt("Please enter your gender: Male or Female?");
+    if (gender.toLowerCase() === "female") {
+        alert("You are a girl 👧🏼");
+    } else if (gender.toLowerCase() === "male") {
+        alert("You are a boy 👦🏽");
+    } else {
+        alert ("Invalid input. Please enter Male or Female");
+    }
 }
