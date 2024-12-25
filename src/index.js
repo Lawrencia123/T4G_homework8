341ffc46be46d2ad58dc4fd44388bcdf523e91ee
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-25 17:49:36 +0000

    made changes

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// function checkGender (){
+// let gender = prompt("What is your gender?");
+// if (gender == "female"){
+//     alert(`You are a girl!👧`);
+// }else if(gender == "male"){
+//     alert(`You are a boy! 👦`);
+//     }else{
+//     alert(`Enter your gender`);
+//     } 
+// }
+// checkGender();
+
+
+function checkGender() {
+    let gender = prompt("Please enter your gender (Male or Female):");
+    // gender will store the value the user enter (Male/Female)
+   if gender = (gender.toLowerCase() === "female") {
+alert("You are a girl!")
+    } 
+    if (gender == "female");
+    alert(`You are a ${gender}`);
+}else if (gender == male) {
+    alert(`You are a ${gender}`);  
+    // or alert(`You are a male`)
+}else{
+    alert("invalid input. Please enter 'male' or 'female'");
+}
